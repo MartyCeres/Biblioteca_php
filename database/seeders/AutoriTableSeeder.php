c84f154c935c6760bbaf5fa3a8c8abf9a3946a51
Martina Ceresoli <martina.ceresoli@edu.unife.it>
2022-12-09 17:39:58 +0100

    modifica commit - creazione seeder Autori

---
+++ database/seeders/AutoriTableSeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class AutoriTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'ciao',
+            'cognome' => 'sono'
+        ]);
+
+       
+       
+    }
+}
