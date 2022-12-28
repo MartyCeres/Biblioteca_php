0eecdc59d43a6db0f88cc4e8f7713489a5225ddb
Laura Ferrieri <laura.ferrieri@edu.unife.it>
2022-12-28 16:31:32 +0100

    compilato autori seeder

--- database/seeders/AutoriTableSeeder.php
+++ database/seeders/AutoriTableSeeder.php
@@ -17,8 +17,56 @@ class AutoriTableSeeder extends Seeder
         //
         DB::table("Autori")->insert([
 
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
+            'nome'=> 'J. K.',
+            'cognome' => 'Rowling'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'Dante',
+            'cognome' => 'Alighieri'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'J. R. R.',
+            'cognome' => 'Tolkien'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'Stephenie',
+            'cognome' => 'Meyer'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'C. S.',
+            'cognome' => 'Lewis'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'Nicholas',
+            'cognome' => 'Sparks'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'Umberto',
+            'cognome' => 'Eco'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'Suzanne',
+            'cognome' => 'Collins'
+        ]);
+
+        DB::table("Autori")->insert([
+
+            'nome'=> 'Rick',
+            'cognome' => 'Riordan'
         ]);
 
         DB::table("Autori")->insert([
@@ -27,55 +75,7 @@ class AutoriTableSeeder extends Seeder
             'cognome' => 'sono'
         ]);
 
-        DB::table("Autori")->insert([
 
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
 
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-        DB::table("Autori")->insert([
-
-            'nome'=> 'ciao',
-            'cognome' => 'sono'
-        ]);
-
-       
-       
     }
 }
