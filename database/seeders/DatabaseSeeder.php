0f53c2ed4283341a7eba92b93f39e874114a4587
Laura Ferrieri <laura.ferrieri@edu.unife.it>
2022-12-29 16:17:59 +0100

    modifica database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(AutoriTableSeeder::class);
+        $this->call(EditoriTableSeeder::class);
+        $this->call(LettoriTableSeeder::class);
+        $this->call(PosizioniTableSeeder::class);
+        $this->call(LibroTableSeeder::class);
     }
 }
