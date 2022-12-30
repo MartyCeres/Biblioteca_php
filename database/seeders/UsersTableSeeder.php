6467aab82b29e4a02901bdea7d73b8fd2263edae
Martina Ceresoli <martina.ceresoli@edu.unife.it>
2022-12-30 10:06:09 +0100

    seeder Users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("users")->insert([
+
+            'name'=> 'Laura',
+            'email' => 'laura.ferrieri@edu.unife.it',
+            'password' => 'laura',
+            'role' => 'admin'
+        ]);
+
+        DB::table("users")->insert([
+
+            'name'=> 'Martina',
+            'email' => 'martina.ceresoli@edu.unife.it',
+            'password' => 'martina',
+            'role' => 'admin'
+        ]);
+
+        DB::table("users")->insert([
+
+            'name'=> 'Giulia',
+            'email' => 'giulia_guest@gmail.com',
+            'password' => 'giulia',
+            'role' => 'guest'
+        ]);
+
+
+    }
+}
