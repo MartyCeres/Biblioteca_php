4dd163b044496965bf570d238a40bdaca8750034
Martina Ceresoli <martina.ceresoli@edu.unife.it>
2022-12-30 10:17:40 +0100

    seeder prestiti

---
+++ database/seeders/PrestitiTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Lettore;
+use App\Models\Libro;
+use DB;
+use Illuminate\Database\Seeder;
+
+class PrestitiTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("users")->insert([
+
+            'libro_id' => Libro::where('titolo', 'Harry Potter e il calice di fuoco')->value('id'),
+            'lettore_id'=> Lettore::where('cognome', 'Verdi')->value('id'),
+        ]);
+
+        DB::table("users")->insert([
+
+            'libro_id' => Libro::where('titolo', 'Divina Commedia')->value('id'),
+            'lettore_id'=> Lettore::where('cognome', 'Rossi')->value('id'),
+        ]);
+
+        DB::table("users")->insert([
+
+            'libro_id' => Libro::where('titolo', 'Le due torri')->value('id'),
+            'lettore_id'=> Lettore::where('cognome', 'Neri')->value('id'),
+        ]);
+
+    }
+}
