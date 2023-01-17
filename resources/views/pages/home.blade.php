a06a9e76c418948c566db45d94bdbbd738115cd5
Laura Ferrieri <laura.ferrieri@edu.unife.it>
2023-01-17 11:27:15 +0100

    modificata design pagina home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,16 @@
 
 @section('content')
 <header>
-    
+
 </header>
 <div class="container">
-    <h1> Titolo </h1>
+    <blockquote class="blockquote text-end">
+    <p><h2>Nel dubbio, vai in biblioteca.</h2></p>
+    <footer class="blockquote-footer">J.K. ROWLING</footer>
+  </blockquote>
+    <img class="img-fluid border border-3 border-warning" src="/images/biblio1.jpg" width="1100" height="500">
+</div>
+<div>
+    <br>
 </div>
 @endsection
