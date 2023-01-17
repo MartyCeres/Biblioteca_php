18ae213ed760f7d884d77e668297a34b4d84f862
Laura Ferrieri <laura.ferrieri@edu.unife.it>
2023-01-17 11:46:28 +0100

    modificata pagina libri

--- resources/views/pages/libri.blade.php
+++ resources/views/pages/libri.blade.php
@@ -4,7 +4,15 @@
 <header>
 
 </header>
-<div class="container">
-    <h1> Titolo Libri </h1>
-</div>
+<br>
+<div class="container row">
+    <div class="col-sm-6 text-dark">
+        <h1>Titolo Libri</h1>
+    </div>
+    <div class="col-sm-6">
+        <img src="/images/libri1.jpg" class="rounded float-end" width="300">
+    </div>
+    </div>
+  </div>
+
 @endsection
