37170232413cbd8599549bc99b25cd2fd273d319
Laura Ferrieri <laura.ferrieri@edu.unife.it>
2023-01-17 11:30:09 +0100

    modificata immagine pagina home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
     <p><h2>Nel dubbio, vai in biblioteca.</h2></p>
     <footer class="blockquote-footer">J.K. ROWLING</footer>
   </blockquote>
-    <img class="img-fluid border border-3 border-warning" src="/images/biblio1.jpg" width="1100" height="500">
+    <img class="img-fluid border border-3 border-warning" src="/images/biblio1.jpg">
 </div>
 <div>
     <br>
