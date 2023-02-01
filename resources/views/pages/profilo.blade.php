c088db1c651343f4d6225e68e59eed3d4d1d2fa5
Martina Ceresoli <martina.ceresoli@edu.unife.it>
2023-02-01 13:13:02 +0100

    Modifiche profilo.blade

--- resources/views/pages/profilo.blade.php
+++ resources/views/pages/profilo.blade.php
@@ -4,19 +4,28 @@
 <div class="center">
     <br>
 <h3>Gestione profilo</h3>
-<div class="row-full">
+<div class="container row">
+<div class="row">
     <div class="col-lg col-md-6 mb-4">
         <form class="form-signin" method="POST" autocomplete="off" action="{{ route('modificaUtente') }}">
             @csrf
             <h6>Indirizzo email:</h6>
             <input type="email" name="email" class="form-control" value="{{ Auth::user()->email }}" placeholder="Indirizzo email">
+            <br>
             <h6>Nome:</h6>
             <input type="text" name="name" class="form-control" value="{{ Auth::user()->name }}" placeholder="Nome">
+            <br>
             <h6>Ruolo:</h6>
-            <span class="border border-success rounded-2 w-50">@if(Auth::user()->hasRole('admin')) admin @endif @if(Auth::user()->hasRole('guest')) guest @endif</span>
-            <button class="btn btn-lg btn-block text-uppercase btn-color" type="submit">Modifica</button>
+            <h4>
+                <span class="badge bg-secondary">@if(Auth::user()->hasRole('admin')) admin @endif @if(Auth::user()->hasRole('guest')) guest @endif</span>
+                <button class="btn btn-block text-uppercase btn-outline-success float-end" type="submit">Modifica</button>
+            </h4>
         </form>
     </div>
+    <div class="col-lg col-md-6 mb-4">
+        <img src="/images/IconaUtente1.png" class="rounded float-end" width="300" >
+    </div>
+</div>
 </div>
 </div>
 @endsection
